5b0e3a66f5bacef6f1f17d60b6a10ba212c3ac7c
ilia <il.vsl0110@gmail.com>
2023-05-22 22:37:54 +0300

    add base generation

--- passfinder/recomendations/service/mapping/mapping.py
+++ passfinder/recomendations/service/mapping/mapping.py
@@ -6,21 +6,29 @@ plays_mapping = None
 excursion_mapping = None
 concert_mapping = None
 
+
+def build_dict(list_mapping):
+    mapping = {}
+    for idx, elem in enumerate(list_mapping):
+        mapping.update({elem: idx})
+    return mapping
+
+
 with open('passfinder/recomendations/service/mapping/attractions.pickle', 'rb') as file:
-    attraction_mapping = pickle.load(file)
+    attraction_mapping = build_dict(pickle.load(file))
 
 
 with open('passfinder/recomendations/service/mapping/kino.pickle', 'rb') as file:
-    cinema_mapping = pickle.load(file)
+    cinema_mapping = build_dict(pickle.load(file))
 
 
 with open('passfinder/recomendations/service/mapping/spektakli.pickle', 'rb') as file:
-    plays_mapping = pickle.load(file)
+    plays_mapping = build_dict(pickle.load(file))
 
 
 with open('passfinder/recomendations/service/mapping/excursii.pickle', 'rb') as file:
-    excursion_mapping = pickle.load(file)
+    excursion_mapping = build_dict(pickle.load(file))
 
 
 with open('passfinder/recomendations/service/mapping/concerts.pickle', 'rb') as file:
-    concert_mapping = pickle.load(file)
+    concert_mapping = build_dict(pickle.load(file))
