a94771d421c2559cfc9d5db12c8fe562ec75b86f
Alexander-D-Karpov <alexandr.d.karpov@gmail.com>
2023-05-26 01:45:14 +0300

    loaded hotels

---
+++ parsers/osm/guest_house.py
@@ -0,0 +1,39 @@
+import json
+
+with open("data/osm/hostel_2.json") as f:
+    data = json.load(f)
+
+
+ress = []
+
+for el in data["elements"]:
+    if "tags" in el and "name" in el["tags"] and "lat" in el and "lon" in el:
+        info = el["tags"]
+        if "tourism" in info:
+            del info["tourism"]
+        res = {
+            "title": info["name:ru"] if "name:ru" in info else info["name"],
+            "type": "guest_house",
+            "parser_source": "openstreetmap.org",
+            "lat": el["lat"],
+            "lon": el["lon"],
+            "extra_kwargs": info,
+        }
+        if "rooms" in info:
+            res["rooms"] = {"amount": info["rooms"]}
+
+        if "description" in info:
+            res["description"] = info["description"]
+
+        if "email" in info:
+            res["email"] = info["email"]
+
+        if "stars" in info:
+            res["stars"] = int(float(info["stars"]))
+        else:
+            res["stars"] = 0
+        ress.append(res)
+
+
+def get_hostels():
+    return ress
