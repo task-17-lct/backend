7668254c057eb3a77f37eef933fd33c2a139f957
ilia <il.vsl0110@gmail.com>
2023-05-28 16:52:03 +0300

    add recs

--- passfinder/recomendations/models.py
+++ passfinder/recomendations/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from passfinder.users.models import User
-from passfinder.events.models import Event, Hotel, Restaurant
+from passfinder.events.models import Event, Hotel, Restaurant, BasePoint
+from passfinder.events.api.serializers import ObjectRouteSerializer
 from django.contrib.postgres.fields import ArrayField
 
 
@@ -47,32 +48,58 @@ class UserPreferences(models.Model):
         base_field=models.IntegerField(), null=True, blank=True
     )
 
+    prefered_other = models.ManyToManyField(Event, related_name='other_users')
+    prefered_hotels = models.ManyToManyField(Hotel, related_name='hotels_user')
+    prefered_restaurants = models.ManyToManyField(Restaurant, related_name='restaurant_user')
+
+    def add_point(self, point: BasePoint):
+        print(point, type(point))
+        if isinstance(point, Event):
+            event = point
+            if event.type == 'play':
+                self.preferred_concerts.add(event)
+            elif event.type == 'movie':
+                self.preffered_movies.add(event)
+            elif event.type == 'attraction':
+                self.prefferred_attractions.add(event)
+            elif event.type == 'museum':
+                self.prefferred_museums.add(event)
+            else:
+                self.prefered_other.add(event)
+            self.save()
+        if isinstance(point, Hotel):
+            self.prefered_hotels.add(point)
+            self.save()
+        if isinstance(point, Restaurant):
+            self.prefered_restaurants.add(point)
+            self.save()
+    
+
+    def get_points(self):
+        points = [
+            *list(self.preffered_plays.all()),
+            *list(self.prefered_hotels.all()),
+            *list(self.prefered_restaurants.all()),
+            *list(self.preferred_concerts.all()),
+            *list(self.preffered_movies.all()),
+            *list(self.prefferred_attractions.all()),
+            *list(self.prefferred_museums.all()),
+            *list(self.prefered_other.all()),
+        ]
+        return list(
+            map(
+                lambda x: ObjectRouteSerializer(x).data, 
+                points
+            )
+        )
+
+        
 
 class NearestEvent(models.Model):
     event = models.ForeignKey(
         Event, on_delete=models.CASCADE, related_name="nearest_model_rel"
     )
     nearest = models.ManyToManyField(Event, related_name="nearest_model_rev_rel")
-
-
-"""
-from passfinder.recomendations.service.service import generate_tour
-
-from passfinder.users.models import User
-
-from passfinder.events.models import City
-from datetime import datetime
-
-start_date = datetime(year=2023, month=6, day=10)
-end_date = datetime(year=2023, month=6, day=13)
-
-c = City.objects.get(title='Таганрог')
-
-u = User.objects.all()[0]
-
-generate_tour(u, c, start_date, end_date)
-
-"""
 
 
 class NearestHotel(models.Model):
